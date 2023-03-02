08689c915d7f02a7f2140e013fa766de172c7749
Bogatyrev R <r.pittchai@gmail.com>
2023-03-02 19:18:30 +0300

    020323/Ant

---
+++ app/utils.py
@@ -0,0 +1,32 @@
+import os
+import sqlite3
+
+
+def create_db() -> None:
+    """
+    To create a database.
+    Для создания БД.
+    """
+    con = sqlite3.connect("engDB.db")
+    cur = con.cursor()
+    table_db: dict = {
+        'verbs': ('eng_verb', 'rus_verb'),
+        'nouns': ('eng_noun', 'rus_noun'),
+        'adjectives': ('eng_adjective', 'rus_adjective'),
+    }
+    for key, value in table_db.items():
+        column_eng, column_rus = value
+        cur.execute(f'CREATE TABLE {key} ({column_eng} '
+                    f'NOT NULL, {column_rus} NOT NULL)')
+    con.close()
+
+
+def connect_sql() -> None:
+    """
+    Checking for the presence of a database.
+    Creating, reading or writing to the database.
+    Проверка на наличие БД.
+    Создание, чтение или запись в БД.
+    """
+    if not os.path.exists("engDB.db"):
+        create_db()
